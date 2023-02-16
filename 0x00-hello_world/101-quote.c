b66a1bdb2095848af843b15dd753b312250ea2f6
zamzam <49584210+SammyThige@users.noreply.github.com>
2023-02-16 18:43:29 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-    static const char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    fwrite(text, sizeof(text) - 1, 1, stdout); 
-    return (1);
+   static const char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+   fwrite(text, sizeof(text) - 1, 1, stdout); 
+   return (1);
 }
