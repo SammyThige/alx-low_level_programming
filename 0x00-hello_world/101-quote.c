c6ef506efc989fcd3d1348ee818c88fab1ee8460
SammyThige <sammythige26@gmail.com>
2023-02-16 07:36:16 -0800

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description - prints text without puts or prinf
+ *
+ * Return: 1 (success)
+*/
+
+int main(void)
+{
+    static const char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    fwrite(text, sizeof(text) - 1, 1, stdout); 
+    return (1);
+}
