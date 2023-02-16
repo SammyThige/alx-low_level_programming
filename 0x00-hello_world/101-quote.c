de3214f84d5ceae08c797df020702b582c830ebc
SammyThige <sammythige26@gmail.com>
2023-02-16 19:21:44 +0300

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@
 
 int main(void)
 {
-  static const char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  fwrite(text, sizeof(text) - 1, 1, stdout); 
-  return (1);
+	 char text[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+
+	 fwrite(text, sizeof(text) - 1, 1, stdout);
+
+	return (1);
 }
