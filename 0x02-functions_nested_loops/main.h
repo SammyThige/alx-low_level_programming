1d2e30456b6c2161b31c00502fee3f2633ae5cce
SammyThige <sammythige26@gmail.com>
2023-02-21 21:56:42 +0300

    c files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+  * print_alphabet - function
+  *
+  * Description - prints alphabet
+  *
+  * Return: void no return type
+  */
+
+void print_alphabet(void);
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_to_99(int n);
+
+#endif
