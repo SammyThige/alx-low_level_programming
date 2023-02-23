d1276fe353f31589278aa66b8a5e709e3f1a61e3
SammyThige <sammythige26@gmail.com>
2023-02-23 21:19:36 +0300

    correction 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j = 0;
 
-	for (i = 0; i < 10; i++)
+	for (; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j > 9)
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
