db6604b6e26e2f32245c7a0a92a212735692c29f
SammyThige <sammythige26@gmail.com>
2023-03-03 00:10:06 +0300

    number 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,39 +3,26 @@
 /**
  * leet - encodes a string into 1337
  *
- * @string: string to encode
+ * @s: string to encode
  *
  * Return: character
  */
 
-char *leet(char *string)
+char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j;
+	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-	while (string[i] != 0)
+	while (s[i])
 	{
-		if (string[i] == 'a' || string[i] == 'A')
-		{
-			string[i] = '4';
-		}
-		else if (string[i] == 'e' || string[i] == 'E')
-		{
-			string[i] = '3';
-		}
-		else if (string[i] == 'o' || string[i] == 'O')
-		{
-			string[i] = '0';
-		}
-		else if (string[i] == 't' || string[i] == 'T')
-		{
-			string[i] = '7';
-		}
-		else if (string[i] == 'l' || string[i] == 'L')
-		{
-			string[i] = '1';
-		}
+		for (j = 0; j < 10; j++)
+			if (s[i] == str[j])
+				s[i] = subs[j];
+
 		i++;
 	}
-	return (string);
+
+	return (s);
 }
 
