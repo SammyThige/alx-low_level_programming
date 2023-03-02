d5282285677155f00c8c7a00b642d1897275e178
SammyThige <sammythige26@gmail.com>
2023-03-02 23:46:20 +0300

    number 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,15 +15,25 @@ char *leet(char *string)
 	while (string[i] != 0)
 	{
 		if (string[i] == 'a' || string[i] == 'A')
+		{
 			string[i] = '4';
+		}
 		else if (string[i] == 'e' || string[i] == 'E')
+		{
 			string[i] = '3';
+		}
 		else if (string[i] == 'o' || string[i] == 'O')
+		{
 			string[i] = '0';
+		}
 		else if (string[i] == 't' || string[i] == 'T')
+		{
 			string[i] = '7';
+		}
 		else if (string[i] == 'l' || string[i] == 'L')
+		{
 			string[i] = '1';
+		}
 		i++;
 	}
 	return (string);
