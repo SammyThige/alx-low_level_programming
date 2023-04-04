fedab2292a0d36bcc3889401c4338c29e56742a7
SammyThige <sammythige26@gmail.com>
2023-04-04 22:12:59 +0300

    trial correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,63 @@
+
+ 
+
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - A function that inserts node at given index
- * @head: A pointer to listint_t structure
- * @idx: The index of the list
- * @n: An integer data for new node
- * Return: The address to new node at specified index, or NULL if it failed
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to the head of the list
+ * @idx: index of the list where the new node should be added.
+ * Index starts at 0
+ * @n: integer to be added to the new node
+ *
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *new_node, *temp;
+	listint_t *new_node, *prev_node;
 
 	if (head == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		new_node->n = n;
-		return (new_node);
-	}
-
+	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		new_node->n = n;
 		*head = new_node;
 		return (new_node);
 	}
-
-	temp = *head;
-	while (i < idx)
+	prev_node = get_nodeint_at_index(*head, idx - 1);
+	if (prev_node == NULL)
 	{
-		temp = temp->next;
-		i++;
+		free(new_node);
+		return (NULL);
 	}
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
 	return (new_node);
+}
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to the head of the list
+ * @index: index of the node, starting at 0
+ *
+ * Return: the nth node of a listint_t linked list. If the node does not exist,
+ * return NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
 }
