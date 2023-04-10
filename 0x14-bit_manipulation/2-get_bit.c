ebaa175f68b07bdd37e1ba553d038ea88011e211
SammyThige <sammythige26@gmail.com>
2023-04-10 18:10:40 +0300

    number 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,14 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
+	if (index > 63)
 		return (-1);
-
-	if ((n & max))
-		return (1);
-	else
-		return (0);
+	return ((n >> index) & 1);
 }
