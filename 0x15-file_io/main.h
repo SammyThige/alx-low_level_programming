f97c42992435923d3699aac23f92323ec05d2a68
SammyThige <sammythige26@gmail.com>
2023-04-10 20:31:26 +0300

    3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,9 @@
 #include <sys/stat.h>
 #include <sys/uio.h>
 
+#define BUF_SIZE 1024
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
