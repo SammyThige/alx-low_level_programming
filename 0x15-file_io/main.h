13f7aa7a62ff4ba05bbc25b8fe84d94767e5d7d8
SammyThige <sammythige26@gmail.com>
2023-04-10 19:14:46 +0300

    main. h correction

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int remove_file(const char *filename);
 
-#end
+#endif
