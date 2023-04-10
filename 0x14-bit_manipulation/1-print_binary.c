6ea89eb54ef20870b228b02d1a7eeb67cf452645
SammyThige <sammythige26@gmail.com>
2023-04-10 18:05:10 +0300

    1 correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,22 +8,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0;
-    int i = 0, j = 0;
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (n > 0)
-	{
-		flag = n & 1;
-		if (flag == 1)
-			j = 1;
-		if (j == 1)
-			_putchar(flag + '0');
-		n = n >> 1;
-		i++;
-	}
 }
